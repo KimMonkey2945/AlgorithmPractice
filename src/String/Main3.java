653b9f853b458fcc619749b2b9d4e63b84cd20c5
김용브 <hvyongkim2@naver.com>
2022-02-26 18:03:08 +0900

    [02-26]

--- src/String/Main3.java
+++ src/String/Main3.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class Main3 {
 	
-	public String soultion(String str) {
+//	public String soultion(String str) {
 //		5. 특정 문자 뒤집기	
 		/*	String result;
 			char [] s = str.toCharArray();
@@ -29,6 +29,7 @@ public class Main3 {
 			result = String.valueOf(s);
 			return result;
 	*/
+		/*
 		String result = "";
 		
 		for(int i = 0; i < str.length(); i++) {
@@ -37,11 +38,15 @@ public class Main3 {
 			}
 		} return result;
 		
+		*/
 		
+//	}
+	
+//	7번 이렇게 boolean 타입의 클래스를 따로 만들어서 
+//	public static boolean solution(String str) {
+//		return str.equals(new StringBuilder(str).reverse().toString());
+//	}
 		
-	}
-	
-	
 	public static void main(String[] args) {
 
 		Scanner scan = new Scanner(System.in);
@@ -55,13 +60,43 @@ public class Main3 {
 		
 //		6.중복문자 제거
 		
+		/*
 		String str = scan.nextLine();
 		Main3 A = new Main3();
 		
 		scan.close();
 		System.out.println(A.soultion(str));
+		*/
 		
 		
+		
+//		7.회문 문자열 
+		//대소문자 구분 시 equalsignorecase를 사용 할 수도 있음.
+//		String input = scan.nextLine();
+//		
+//		String str = input.toUpperCase();
+//		
+//		scan.close();
+//		if(solution(str)){
+//			System.out.println("YES");			
+//		}else {
+//			System.out.println("NO");
+//		}
+		
+//		8.유효한 팰린드롬()
+		
+		String str = scan.nextLine(); //꺽쇠는 부정을 나타냄
+		scan.close();
+
+		str = str.toUpperCase().replaceAll("[^A-Z]", "");
+		
+		String b = new StringBuilder(str).reverse().toString();
+		
+		if(str.equals(b)) {
+			System.out.println("YES");
+		}else {
+			System.out.println("NO");
+		}	
 	}
 
 }
