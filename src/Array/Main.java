f15aac73be2641898fda1d9c7dd50ffe5b4d87d4
김용브 <hvyongkim2@naver.com>
2022-03-04 11:57:40 +0900

    [03-04]

--- src/Array/Main.java
+++ src/Array/Main.java
@@ -18,6 +18,7 @@ public class Main {
 		*/
 	
 //		2.보이는 학생
+	/*
 	public int solution(int n, int[] arr){
 		
 		int count = 1;
@@ -30,7 +31,7 @@ public class Main {
 		}
 		return count;
 	}
-	
+	*/
 	
 
 	public static void main(String[] args) {
@@ -52,7 +53,7 @@ public class Main {
 	*/
 		
 //		2. 보이는 학생
-		
+	/*	
 		int n = scan.nextInt();
 		int[] arr = new int[n];
 		
@@ -61,6 +62,72 @@ public class Main {
 		}
 	
 		System.out.println(R.solution(n, arr));
+	*/
+		
+//		3.가위 바위 보
+		/*
+		int n = scan.nextInt();
+		
+		int[] a = new int[n];
+		int[] b = new int[n];
+		
+		for(int i = 0; i < n; i++) {
+			a[i] = scan.nextInt(); 
+		}
+		
+		for(int i = 0; i < n; i++) {
+			b[i] = scan.nextInt(); 
+		}
+		
+		
+		for(int i = 0; i < n; i++) {
+			if((a[i] == 1 && b[i] == 2) || (a[i] == 2 && b[i] == 3) || (b[i] == 1 && a[i] == 3)) {
+				System.out.println("B");
+			}else if(a[i] == b[i]) {
+				System.out.println("D");
+			}else {
+				System.out.println("A");
+			}
+		}
+	*/	
+		
+//		4.피보나치 수열
+		/*
+		int n = scan.nextInt();
+		
+		int[] a = new int[n];
+		a[0] = 1;
+		a[1] = 1;
+		a[2] = 2;
+		
+		for(int i = 3; i < n; i++) {
+			a[i] = a[i-1] + a[i-2]; 
+		}
+		
+		for(int b: a) {
+			System.out.print(b + " ");
+		}
+		*/
+		
+//		배열을 사용하지 않고 풀어라
+		
+		int n = scan.nextInt();
+		int a = 1, b = 1, c;
+		System.out.print(a + " " + b + " ");
+		for(int i = 2; i < n; i++) {
+			c = a + b;
+			System.out.print(c + " ");
+			a = b;
+			b = c;
+		}
+		
+		
+		
+		
+		
+		
+		
+		
 		
 		
 		
